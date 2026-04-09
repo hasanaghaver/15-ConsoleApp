9a06041af9f801775ae250ac08e6baef3ba51c09
hasanagaver <hasanaa-apa202@gmail.com>
2026-04-09 20:38:28 +0400

    Create app and DomainLayer

---
+++ Course/DomainLayer/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public class Group : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public int Room { get; set; }
+    }
+}
